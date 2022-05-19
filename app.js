847069c1141dd52f18977656ca5c086ef6f3bf1b
Muhammad-yogi <yogikgaek@gmail.com>
2022-05-19 21:49:49 +0700

    test update

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use(`/api/v3`, productRouterV3);
 app.use(`/api/v4`, productRouterV4);
 app.use(logger(`dev`));
 
-app.use('/public', express.static(path.join(__dirname, 'uploads')));
+app.use('/public', express.static(path.join(__dirname, 'uploads'))); 
 app.use(`/`, (req, res, next) => {
     res.status(404)
     // use tag html
     res.send(`<h1> Page not found </h1>`); 
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
