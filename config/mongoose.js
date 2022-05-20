c11ddc4e96a856e262ae490b65823c3c0f561621
Muhammad-yogi <yogikgaek@gmail.com>
2022-05-20 15:50:52 +0700

    update mongo atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require(`mongoose`);
 
-mongoose.connect(`mongodb://yogi:eduwork@localhost:27017/eduwork-mongoose?authSource=admin`);
+mongoose.connect(`mongodb+srv://yogi-admin:eduwork@cluster0.otzzu.mongodb.net/eduwork-mongoose?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 db.on(`error`, console.error.bind(console, `connection error: `));
