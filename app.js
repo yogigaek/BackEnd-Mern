aec4aff3963704b78a54c4cea6edbcb9261ae7c5
Muhammad-yogi <yogikgaek@gmail.com>
2022-05-22 01:37:20 +0700

    update second

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const bodyParser = require(`body-parser`);
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(`/api/v3`, productRouterV3);
 app.use(`/api/v4`, productRouterV4);
-app.use(logger(`dev`));
+app.use(cors());
 
+app.use(logger(`dev`));
 app.use('/public', express.static(path.join(__dirname, 'uploads'))); 
 app.use(`/`, (req, res, next) => {
     res.status(404)
@@ -22,12 +22,5 @@ app.use(`/`, (req, res, next) => {
     res.send(`<h1> Page not found </h1>`); 
 });
 
-if(process.env.NODE_ENV === `production`) {
-    app.use(express.static('FrontEnd-Mern/build'));
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'FrontEnd-Mern', 'build', 'index.html'));
-    })
-}
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
