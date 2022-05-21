5cd4be5870ff5775ab83b8f51851d7359bd42c31
Muhammad-yogi <yogikgaek@gmail.com>
2022-05-21 11:15:40 +0700

    firts

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const cors = require(`cors`);
 const logger = require(`morgan`);
 const productRouterV3 = require(`./productV3/routes`);
 const productRouterV4 = require(`./productV4/routes`); 
-const BodyParser = require(`body-parser`);
+const bodyParser = require(`body-parser`);
 
-app.use(BodyParser.json()); // for parsing application/json
-app.use(BodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(`/api/v3`, productRouterV3);
 app.use(`/api/v4`, productRouterV4);
@@ -22,5 +22,12 @@ app.use(`/`, (req, res, next) => {
     res.send(`<h1> Page not found </h1>`); 
 });
 
-const PORT = process.env.PORT || 3000;
+if(process.env.NODE_ENV === `production`) {
+    app.use(express.static('frontend/build'));
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
